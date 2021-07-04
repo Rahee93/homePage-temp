696279ab177d5fccc9d7a566d5aef2b8b52d1583
Zihan Bai <bai00032@algonquinlive.com>
2021-07-03 21:51:38 -0400

    add google map

--- microbit-temperature-collector/react/src/pages/map-page/MapPage.js
+++ microbit-temperature-collector/react/src/pages/map-page/MapPage.js
@@ -1,46 +1,103 @@
 import Typography from '@material-ui/core/Typography';
 import GoogleMapReact from 'google-map-react';
 import React, { Component } from 'react';
-
-const defaultZoom = 10;
+import { StylesProvider } from '@material-ui/core/styles';
+import Header from "../../components/header/Header";
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { firestore } from "../../firebase/firebase";
+const defaultZoom = 12;
 const OTTAWA_CENTER = { lat: 45.4215, lng: -75.6972 };
 
 class MapPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hoverOverSchool: '',
-      mapHeight: 0,
-    };
+
+  state = {
+    currentSelectedSchoolId: '',
+    mapHeight: 0,
+    schoolData: {
+      loadingSchooleList: false,
+      schooleList: []
+    },
+    temperatureData: {
+      loadingTemperatureData: false,
+      temperatureDataBySchool: []
+    }
+  }
+
+  fetchData = async () => {
+    // loading the school list
+    this.setState({
+      schoolData: {
+        ...this.state.schoolData,
+        loadingSchooleList: true
+      }
+    });
+
+    // TODO store the schoolListSnapshot so do not need to fetch every time
+    const schoolListSnapshot = await firestore
+      .collection("temperature-collector-school-list")
+      .orderBy("School_Name", "asc")
+      .get();
+
+    const schooleList = {};
+    schoolListSnapshot.forEach((doc) => {
+      schooleList[doc.id] = doc.data()
+    });
+    this.setState({
+      schoolData: {
+        schooleList,
+        loadingSchooleList: false
+      }
+    });
+
+    // load temperature data
+    this.setState({
+      temperatureData: {
+        ...this.state.temperatureData,
+        loadingTemperatureData: true
+      }
+    });
+
+    const temperatureDataSnapshot = await firestore
+      .collection("temperature-collector-temperature-data")
+      .get();
+
+    const temperatureDataBySchool = {};
+    temperatureDataSnapshot.forEach((doc) => {
+      temperatureDataBySchool[doc.id] = doc.data()
+    });
+
+    this.setState({
+      temperatureData: {
+        temperatureDataBySchool,
+        loadingSchooleList: false
+      }
+    });
   }
 
   componentWillUnmount() {
-    this.clearOttawaSchoolMarkers();
+    this.clearSchoolMarkers();
   }
 
   componentDidMount() {
+    this.fetchData();
     this.updateMapHeight();
     window.addEventListener('resize', this.updateMapHeight);
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.currentCategory !== prevProps.currentCategory) {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.schoolData.schooleList.length !== prevState.schoolData.schooleList.length) {
       // need to wait map to load
       if (this.map && this.maps) {
         this.schoolsToMarkers();
         this.panToOttawaCenter();
       }
-    } else if (this.props.ottawaSchools.length !== prevProps.ottawaSchools.length) {
-      // need to wait map to load
-      if (this.map && this.maps) {
-        this.schoolsToMarkers();
-        this.panToOttawaCenter();
-      }
-    } else if (this.props.currentSelectedSchool.NAME !== prevProps.currentSelectedSchool.NAME) {
+    } else if (this.state.currentSelectedSchoolId !== prevState.currentSelectedSchoolId) {
       if (this.map) {
         // before select a new school, unselect pre selected school
-        this.unSelectSchool(prevProps.currentSelectedSchool.NAME);
-        this.selectSchool(this.props.currentSelectedSchool.NAME);
+        this.unSelectSchool(prevState.currentSelectedSchoolId);
+        this.selectSchool(this.state.currentSelectedSchoolId);
       }
     }
   }
@@ -56,9 +113,10 @@ class MapPage extends Component {
     }
   };
 
-  onSelectedSchoolChange = newSelectedSchool => {
-    // notify currentSelectedSchool is now newSelectedSchool
-    this.props.onSelectedSchoolChange(newSelectedSchool);
+  onSelectedSchoolChange = newSelectedSchoolId => {
+    this.setState({
+      currentSelectedSchoolId: newSelectedSchoolId
+    });
   };
 
   handleApiLoaded = (map, maps) => {
@@ -67,84 +125,94 @@ class MapPage extends Component {
     this.schoolsToMarkers();
   };
 
-  clearOttawaSchoolMarkers() {
-    if (this.ottawaSchoolMarkers && Object.keys(this.ottawaSchoolMarkers).length > 0) {
-      Object.keys(this.ottawaSchoolMarkers).forEach(name => {
-        this.ottawaSchoolMarkers[name].markerListeners.forEach(listener => listener.remove());
-        this.ottawaSchoolMarkers[name].marker.setMap(null);
-        delete this.ottawaSchoolMarkers[name];
+  clearSchoolMarkers = () => {
+    if (this.schoolMarkers && Object.keys(this.schoolMarkers).length > 0) {
+      Object.keys(this.schoolMarkers).forEach(schoolId => {
+        this.schoolMarkers[schoolId].markerListeners.forEach(listener => listener.remove());
+        this.schoolMarkers[schoolId].marker.setMap(null);
+        delete this.schoolMarkers[schoolId];
       });
     }
-    this.ottawaSchoolMarkers = null;
+    this.schoolMarkers = null;
   }
 
-  panToOttawaCenter() {
+  panToOttawaCenter = () => {
     this.map.setZoom(defaultZoom);
     this.map.panTo(OTTAWA_CENTER);
   }
 
-  schoolsToMarkers() {
-    // clear exsiting markers
-    this.clearOttawaSchoolMarkers();
+  getColorByTemperature = (temeprature) => {
 
-    this.ottawaSchoolMarkers = {};
-    const { ottawaSchools } = this.props;
-    if (ottawaSchools) {
-      ottawaSchools.forEach(ottawaSchool => {
-        const { position, NAME: name, rankInOttawa } = ottawaSchool;
+  }
+  schoolsToMarkers = () => {
+    // clear exsiting markers
+    this.clearSchoolMarkers();
+
+    this.schoolMarkers = {};
+    const schools = this.state.schoolData.schooleList;
+    if (schools) {
+      Object.keys(schools).forEach(schoolId => {
+        const school = schools[schoolId];
+        const position = { lat: school.lat, lng: school.lng };
         const marker = new this.maps.Marker({
-          opacity: 0.7,
+          opacity: 0.8,
           position,
-          label: `${rankInOttawa}`,
-          title: name,
+          title: school.School_Name,
           map: this.map,
         });
         const markerListeners = [];
         markerListeners.push(
-          marker.addListener('mouseover', () => {
-            this.setState({
-              hoverOverSchool: name,
-            });
-          }),
-        );
-        markerListeners.push(
-          marker.addListener('mouseout', () => {
-            this.setState({
-              hoverOverSchool: '',
-            });
-          }),
-        );
-        markerListeners.push(
           marker.addListener('click', () => {
-            this.onSelectedSchoolChange(ottawaSchool);
+            this.onSelectedSchoolChange(schoolId);
           }),
         );
 
-        this.ottawaSchoolMarkers[name] = {
+        //Creates Circle objects to display on the map in locations found in database.
+        //Using min and max to determine the colour of the circle with getCircleColour()
+        // new google.maps.Circle({
+        //   strokeColor: getColorByTemperature(citymap[city].minTemp),
+        //   strokeOpacity: 0.8,
+        //   strokeWeight: 2,
+        //   fillColor: getColorByTemperature(citymap[city].minTemp),
+        //   fillOpacity: 0.35,
+        //   map,
+        //   center: citymap[city].location,
+        //   radius: 200,
+        // })
+
+        // new google.maps.Circle({
+        //     strokeColor: getColorByTemperature(citymap[city].maxTemp),
+        //     strokeOpacity: 0.8,
+        //     strokeWeight: 2,
+        //     fillColor: getColorByTemperature(citymap[city].maxTemp),
+        //     fillOpacity: 0.35,
+        //     map,
+        //     center: citymap[city].location,
+        //     radius: 250,
+        // })
+
+        this.schoolMarkers[schoolId] = {
           position,
           marker,
           markerListeners,
         };
-        if (name === this.props.currentSelectedSchool.NAME) {
-          this.selectSchool(name);
-        }
       });
     }
   }
 
-  unSelectSchool(name) {
+  unSelectSchool = (schoolId) => {
     // unselected style
-    if (this.ottawaSchoolMarkers[name]) {
-      const { marker } = this.ottawaSchoolMarkers[name];
-      marker.setOpacity(0.7);
+    if (this.schoolMarkers[schoolId]) {
+      const { marker } = this.schoolMarkers[schoolId];
+      marker.setOpacity(0.8);
       marker.setZIndex();
     }
   }
 
-  selectSchool(name) {
-    if (this.ottawaSchoolMarkers[name]) {
-      const { marker, position } = this.ottawaSchoolMarkers[name];
-      const SELECTION_ZOOM = 12;
+  selectSchool = (schoolId) => {
+    if (this.schoolMarkers[schoolId]) {
+      const { marker, position } = this.schoolMarkers[schoolId];
+      const SELECTION_ZOOM = 15;
       if (this.map.getZoom() < SELECTION_ZOOM) {
         this.map.setZoom(SELECTION_ZOOM);
       }
@@ -157,27 +225,36 @@ class MapPage extends Component {
 
   render() {
     return (
-      <div>
-        <div
-          id="title"
-          style={{
-            marginTop: 5,
-            marginBottom: 5,
-            marginLeft: 6,
-            marginRight: 6,
-          }}
-        >
+      <StylesProvider injectFirst>
+        <Header />
+        <div style={{padding: '0 50px', display: 'flex', justifyContent:'space-between'}}>
+          <Autocomplete
+            value={this.state.currentSelectedSchoolId}
+            getOptionSelected={(option, value) => option.value === value}
+            onChange={(event, newSelectedSchool) => {
+              if(newSelectedSchool) {
+                this.onSelectedSchoolChange(newSelectedSchool.value);
+              } else {
+                this.onSelectedSchoolChange('');
+              }
+            }}
+            id="school"
+            options={Object.keys(this.state.schoolData.schooleList).map((schoolId) => ({value: schoolId, label: this.state.schoolData.schooleList[schoolId].School_Name}))}
+            getOptionLabel={(option) => option && option.label ? option.label : ''}
+            style={{width: '50%' }}
+            renderInput={(params) => <TextField {...params} label="School" variant="outlined" margin="normal" />}
+          />
         </div>
-        <div style={{ height: `${this.state.mapHeight}px`, width: '100%' }}>
+        <div style={{ alignSelf: 'center', height: `${this.state.mapHeight}px`, padding: '0 50px', width: '100%' }}>
           <GoogleMapReact
             defaultZoom={defaultZoom}
             defaultCenter={OTTAWA_CENTER}
-            bootstrapURLKeys={{ key: 'AIzaSyCld6JUO9krxRQ-dAic2r2xdMdWMyzjl80' }}
+            bootstrapURLKeys={{ key: 'AIzaSyCld6JUO9krxRQ-dAic2r2xdMdWMyzjl80'/*'AIzaSyC1gDnhhIAkrOF5abBFwo1ysfwjSxHJBag'*/ }}
             yesIWantToUseGoogleMapApiInternals
             onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
           ></GoogleMapReact>
         </div>
-      </div>
+      </StylesProvider>
     );
   }
 }
