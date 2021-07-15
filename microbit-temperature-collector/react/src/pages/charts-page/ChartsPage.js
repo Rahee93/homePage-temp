54ed14ae64a67330f3b57673c58c393631ca34a1
justinblack0454 <blac0454@algonquinlive.com>
2021-07-15 14:01:04 -0400

    added header pic and adjusted page content

--- microbit-temperature-collector/react/src/pages/charts-page/ChartsPage.js
+++ microbit-temperature-collector/react/src/pages/charts-page/ChartsPage.js
@@ -5,13 +5,19 @@ import Footer from "../../components/footer/Footer";
 import BackToTop from "../../components/scroll-top/ScrollTop";
 import AvgTempLineChart from "./ChartConfig";
 import mockChart from "../../assets/photo/mockchart.PNG";
+import './ChartsPage.scss';
 
 const ChartsPage = () => {
     return (
         <>
         <Header />
-        <img width="900px"style={{flex: '1 1 auto', alignSelf:'center'}} src={mockChart}/>
+        <div className="ChartsPage-banner">
+            <h1 className="ChartsPage-banner__title">Charts</h1>
+        </div>
+        <p className="ChartsPage-content">Below is a line chart depicting the average temperature over time for your school:</p>
         <AvgTempLineChart/>
+        <p className="ChartsPage-content">Below is a mock bar chart (to be replaced with a real chart!):</p>
+        <img width="900px"style={{flex: '1 1 auto', alignSelf:'center'}} src={mockChart}/>
         <Footer />
         <BackToTop />
         </>
