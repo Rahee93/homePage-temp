a61ca98480603ae2e38fff357c26fb4a88a93964
justinblack0454 <blac0454@algonquinlive.com>
2021-07-17 13:26:19 -0400

    fixed some html and formatting in code

--- microbit-main/src/pages/tutorials-page/TutorialsPage.js
+++ microbit-main/src/pages/tutorials-page/TutorialsPage.js
@@ -14,7 +14,7 @@ const TutorialsPage = () => {
                     <h1 className="TutorialsPage-banner__title">Tutorials</h1>
                 </div>
                 <div className="TutorialsPage-content">
-                        <div className="TutorialsPage-linkBanner">
+                    <div className="TutorialsPage-linkBanner">
                         <a href="https://makecode.microbit.org/#tutorial:github:justinblack0454/hello-tutorial/tutorial">            
                         <div className="TutorialsPage-imghello">
                             <h2 className="TutorialsPage-banner-title">Hello Tutorial</h2>
@@ -22,13 +22,15 @@ const TutorialsPage = () => {
                         </div>
                         </a> 
                     </div>
-                    <a href="https://makecode.microbit.org/#tutorial:github:afmcdnl/trundle-wheel---tutorial/Tutorial">            
-                    <div className="TutorialsPage-img">
-                        <h2 className="TutorialsPage-banner-title">Trundle Wheel</h2>
-                        <p className="TutorialsPage-banner-description">Using a cardboard wheel and a micro:bit keep track of the distance from one object to another</p>
+
+                    <div className="TutorialsPage-linkBanner"></div>
+                        <a href="https://makecode.microbit.org/#tutorial:github:afmcdnl/trundle-wheel---tutorial/Tutorial">            
+                        <div className="TutorialsPage-img">
+                            <h2 className="TutorialsPage-banner-title">Trundle Wheel</h2>
+                            <p className="TutorialsPage-banner-description">Using a cardboard wheel and a micro:bit keep track of the distance from one object to another</p>
+                        </div>
+                        </a> 
                     </div>
-                    </a> 
-                </div>
                 <Footer />
             </div>  
         </div>
